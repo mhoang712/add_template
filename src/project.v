691fa08167f0ea185610d102c6cdad4c041430f7
mhoang712 <minhhoang17807@gmail.com>
2025-07-02 15:55:00 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -20,7 +20,7 @@ module tt_um_asiclab_example (
     wire reset = ~rst_n;
     assign uio_out = 0;
     assign uio_oe = 0;
-    wire _unuse = &{ena,uio_in,1'b0};
+    wire _unused = &{ena,uio_in,1'b0};
 
     always @(posedge clk or negedge reset) begin
         if(reset) begin
