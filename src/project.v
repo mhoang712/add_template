cb4a1277241d9b50767f79925d0afd8b98f1d8f1
mhoang712 <minhhoang17807@gmail.com>
2025-07-02 15:49:38 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,7 +17,6 @@ module tt_um_asiclab_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-    wire _unused = &{ena, clk, rst_n, 1'b0};
     wire reset = ~rst_n;
     assign uio_out = 0;
     assign uio_oe = 0;
